b9688b76b0520290495d7b828f3b9a6d11e8363b
Christian Kevin Kusuma <73664921+c-kevin-kusuma@users.noreply.github.com>
2023-04-06 15:37:04 -0600

    Add files via upload

--- R/pRoDomo.R
+++ R/pRoDomo.R
@@ -719,7 +719,7 @@ Domo <- setRefClass("Domo",contains='DomoUtilities',
                           batch <- 50 #limit of what the API will return
                           while( n_ret > 0 ){
                             my_batches[[i]] <- httr::content(httr::GET(paste('https://',.self$domain,'/v1/projects/',project_id,'/tasks?offset=',offset,'&limit=',limit,sep=''),my_headers,query=list(sort='name',limit=batch,offset=((i-1)*batch))))
-                            n_ret <- ifelse(length(my_batches[[i]]) < batch,0,1)
+                            n_ret <- ifelse(length(my_batches[[i]]) >= batch,0,1)
                             i <- i + 1
                           }
                           out <- unlist(my_batches,recursive=FALSE)
@@ -766,7 +766,7 @@ Domo <- setRefClass("Domo",contains='DomoUtilities',
                           batch <- 50 #limit of what the API will return
                           while( n_ret > 0 ){
                             my_batches[[i]] <- httr::content(httr::GET(paste('https://',.self$domain,'/v1/projects/','?offset=',offset,'&limit=',limit,sep=''),my_headers,query=list(sort='name',limit=batch,offset=((i-1)*batch))))
-                            n_ret <- ifelse(length(my_batches[[i]]) < batch,0,1)
+                            n_ret <- ifelse(length(my_batches[[i]]) >= batch,0,1)
                             i <- i + 1
                           }
                           out <- unlist(my_batches,recursive=FALSE)
